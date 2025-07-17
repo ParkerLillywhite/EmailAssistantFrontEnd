22fdb1f4af1176ba59c94032dc39de1fde0509d5
Parker Lillywhite <parkerlillywhite@gmail.com>
2025-07-17 16:17:48 -0600

    Adds more modularility

--- src/components/scrollingpage.tsx
+++ src/components/scrollingpage.tsx
@@ -1,6 +1,6 @@
 import React, {ReactNode} from 'react';
 
-interface LayoutProps {
+interface Section {
     leftText?: string;
     leftSVG?: ReactNode;
     leftTextBackground?: ReactNode;
@@ -19,109 +19,101 @@ interface LayoutProps {
     svg?: ReactNode;
 }
 
+interface LayoutProps {
+    sections: Section[]
+    children?: ReactNode
+}
+
 //consider using an Id related to the svg rather than passing in the svg itself as a prop.
 
 const ScrollingPage: React.FC<LayoutProps> = ({ 
-    leftText,
-    leftSVG,
-    leftSVGBackground,
-    leftTextBackground,
-    isLeftTextFirst,
-    mainText,
-    mainSVG,
-    mainSVGBackground,
-    mainTextBackground,
-    isMainTextFirst,
-    rightSVG,
-    rightText,
-    rightSVGBackground,
-    rightTextBackground,
-    isRightTextFirst
-
+    sections,
+    children
 }) => {
-
-    //TODO: better class names!
     
   return (
     <div className="container">
+        { sections.map((section, index) => (
+
         <div className="row main-content">
             <svg />
-            {isLeftTextFirst ? (
+            {section.isLeftTextFirst ? (
                 <>
                     <div className="col-2 left-section-text-first-text">
-                        {leftText && <div className="left-text">{leftText}</div>}
-                        {leftTextBackground && <div className="left-text-background">{leftTextBackground}</div>}
+                        {section.leftText && <div className="left-text">{section.leftText}</div>}
+                        {section.leftTextBackground && <div className="left-text-background">{section.leftTextBackground}</div>}
                     </div>
                     <div className="col-2 left-section-text-first-svg">
-                        {leftSVG && <div className="left-svg">{leftSVG}</div>} 
-                        {leftSVGBackground && <div className="left-svg-background">{leftSVGBackground}</div>}
+                        {section.leftSVG && <div className="left-svg">{section.leftSVG}</div>} 
+                        {section.leftSVGBackground && <div className="left-svg-background">{section.leftSVGBackground}</div>}
                     </div>
                     )
                 </> 
             ) : ( 
                 <>
                     <div className="col-2 right-section-svg-first-svg">
-                        {leftSVG && <div className="left-svg">{leftSVG}</div>} 
-                        {leftSVGBackground && <div className="left-svg-background">{leftSVGBackground}</div>}
+                        {section.leftSVG && <div className="left-svg">{section.leftSVG}</div>} 
+                        {section.leftSVGBackground && <div className="left-svg-background">{section.leftSVGBackground}</div>}
                     </div>
                     <div className="col-2 right-section-svg-first-text">
-                        {leftText && <div className="left-text">{leftText}</div>}
-                        {leftTextBackground && <div className="left-text-background">{leftTextBackground}</div>}
+                        {section.leftText && <div className="left-text">{section.leftText}</div>}
+                        {section.leftTextBackground && <div className="left-text-background">{section.leftTextBackground}</div>}
                     </div>
                 </>
             )
             }
-            {isMainTextFirst ? (
+            {section.isMainTextFirst ? (
                 <>
                     <div className="col-2 main-section-text-first-text">
-                        {mainText && <div className="main-text">{mainText}</div>}
-                        {mainTextBackground && <div className="main-text-background">{mainTextBackground}</div>}
+                        {section.mainText && <div className="main-text">{section.mainText}</div>}
+                        {section.mainTextBackground && <div className="main-text-background">{section.mainTextBackground}</div>}
                     </div>
                     <div className="col-2 main-section-text-first-svg">
-                        {mainSVG && <div className="main-svg">{mainSVG}</div>} 
-                        {mainSVGBackground && <div className="main-svg-background">{mainSVGBackground}</div>}
+                        {section.mainSVG && <div className="main-svg">{section.mainSVG}</div>} 
+                        {section.mainSVGBackground && <div className="main-svg-background">{section.mainSVGBackground}</div>}
                     </div>
                     )
                 </> 
             ) : ( 
                 <>
                     <div className="col-2 main-section-svg-first-svg">
-                        {mainSVG && <div className="main-svg">{mainSVG}</div>} 
-                        {mainSVGBackground && <div className="main-svg-background">{mainSVGBackground}</div>}
+                        {section.mainSVG && <div className="main-svg">{section.mainSVG}</div>} 
+                        {section.mainSVGBackground && <div className="main-svg-background">{section.mainSVGBackground}</div>}
                     </div>
                     <div className="col-2 main-section-svg-first-text">
-                        {mainText && <div className="main-text">{mainText}</div>}
-                        {mainTextBackground && mainSVGBackground && <div className="main-text-background">{mainSVGBackground}</div>}
+                        {section.mainText && <div className="main-text">{section.mainText}</div>}
+                        {section.mainTextBackground && section.mainSVGBackground && <div className="main-text-background">{section.mainSVGBackground}</div>}
                     </div>
                 </>
             )
             }
-            {isRightTextFirst ? (
+            {section.isRightTextFirst ? (
                 <>
                     <div className="col-2 right-section-text-first-text">
-                        {rightText && <div className="right-text">{rightText}</div>}
-                        {rightTextBackground && <div className="main-text-background">{mainSVGBackground}</div>}
+                        {section.rightText && <div className="right-text">{section.rightText}</div>}
+                        {section.rightTextBackground && <div className="main-text-background">{section.mainSVGBackground}</div>}
                     </div>
                     <div className="col-2 right-section-text-first-svg">
-                        {rightSVG && <div className="right-svg">{rightSVG}</div>} 
-                        {rightSVGBackground && <div className="right-svg-background">{rightSVGBackground}</div>}
+                        {section.rightSVG && <div className="right-svg">{section.rightSVG}</div>} 
+                        {section.rightSVGBackground && <div className="right-svg-background">{section.rightSVGBackground}</div>}
                     </div>
                     )
                 </> 
             ) : ( 
                 <>
                     <div className="col-2 right-section-svg-first-svg">
-                        {rightSVG && <div className="right-svg">{rightSVG}</div>} 
-                        {rightSVGBackground && <div className="right-svg-background">{rightSVGBackground}</div>}
+                        {section.rightSVG && <div className="right-svg">{section.rightSVG}</div>} 
+                        {section.rightSVGBackground && <div className="right-svg-background">{section.rightSVGBackground}</div>}
                     </div>
                     <div className="col-2 right-section-svg-first-text">
-                        {rightText && <div className="right-text">{rightText}</div>}
-                        {rightTextBackground && <div className="main-text-background">{mainSVGBackground}</div>}
+                        {section.rightText && <div className="right-text">{section.rightText}</div>}
+                        {section.rightTextBackground && <div className="main-text-background">{section.mainSVGBackground}</div>}
                     </div>
                 </>
             )
             }
         </div>
+        ))}
     </div>
   );
 }
---
+++ src/constants/homepage-constants.ts
@@ -0,0 +1,22 @@
+import Logo from '../logo.svg'
+
+const bunkString = "This is some content I made up here";
+
+export const SECTIONS = [
+    {
+        leftString: bunkString,
+        leftSvg: Logo,
+        leftTextBackground: Logo,
+        leftSvgBackground: Logo,
+        isLeftTextFirst: true,
+        mainText: bunkString,
+        mainTextBackground: Logo,
+        mainSvgBackgroung: Logo,
+        isMainTextFirst: false,
+        rightText: bunkString,
+        rightSVG: Logo,
+        rightTextBackground: Logo,
+        isRightTextFirst: true,
+        
+    }
+]
