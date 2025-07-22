d3f14414aeda87635558899273c16f19c2dc7d8f
Parker Lillywhite <parkerlillywhite@gmail.com>
2025-07-22 15:05:54 -0600

    Adds jitter effect

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,11 +1,35 @@
-import React, {ReactNode} from 'react';
+import React, {ReactNode, useRef, useEffect} from 'react';
+import {ReactComponent as Satellite} from '../assets/satellite.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.scss';
 
 interface LayoutProps {
     children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+
+  const jitterRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    const handleScroll = () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        if (jitterRef.current) {
+          jitterRef.current.classList.add('jitter');
+          setTimeout(() => {
+            jitterRef.current?.classList.remove('jitter');
+          }, 300);
+        }
+      }, 150);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className="container">
       <header className="row app-header">
@@ -15,8 +39,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <div className="col-4 d-flex justify-content-center align-items-center header-center">
             Center
         </div>
-        <div className="col-4 d-flex justify-content-center align-items-center header-right">
-            Right side
+        <div ref={jitterRef} className="col-4 d-flex justify-content-center align-items-center header-right">
+            <Satellite />
         </div>
       </header>
       {children}
